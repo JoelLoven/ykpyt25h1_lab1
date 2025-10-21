7501fd6698ce3dff2bea172f8e055b2300e3c67e
joelloven <joel.loven@storytel.com>
2025-10-21 13:22:18 +0200

    spelling and formating fixes

--- rpc.py
+++ rpc.py
@@ -6,9 +6,9 @@ LOSE = "You lose!"
 TIE = "It's a tie!"
 
 class round_result:
-     def __init__(self,outcome, oponent_pick):
+     def __init__(self,outcome, opponent_pick):
         self.outcome = outcome
-        self.oponent_pick = oponent_pick
+        self.opponent_pick = opponent_pick
 
 
 def get_user_pick_input():
@@ -19,7 +19,7 @@ def get_user_pick_input():
         else:
             print("Invalid input. Try again using rock, paper or scissors.")
 
-def get_oponent_pick():
+def get_opponent_pick():
     return random.choice(["rock", "paper", "scissors"])
 
 def determine_winner(user, computer):
@@ -32,33 +32,33 @@ def determine_winner(user, computer):
     else:
         return LOSE
 
-def clear_consol():
+def clear_console():
     os.system('cls' if os.name=='nt' else 'clear')
 
-def play_roud():
-            computer_pick = get_oponent_pick()
-            user_pick = get_user_pick_input()
-            result = determine_winner(user_pick, computer_pick)
-            return round_result(result, computer_pick)
+def play_round():
+    computer_pick = get_opponent_pick()
+    user_pick = get_user_pick_input()
+    result = determine_winner(user_pick, computer_pick)
+    return round_result(result, computer_pick)
 
 def main():
     print(
         '\033[4m\033[96m' + "Let's play Rock Paper Scissors\033[0m")
     while True:
-        result = play_roud()
+        result = play_round()
         while result.outcome == TIE:
             print(TIE + ", let's play again")
-            result = play_roud()
+            result = play_round()
 
         result_print_color = '\033[92m' if result.outcome == WIN else '\033[91m'
 
-        print(f"\nOponent picked: {result.oponent_pick}, \033[1m{result_print_color}{result.outcome}\033[0m")
+        print(f"\nOpponent picked: {result.opponent}, \033[1m{result_print_color}{result.outcome}\033[0m")
         again = input("\nPlay again? (y/n): ").strip().lower()
         if again != 'y':
             print("Thanks for playing.")
             break
         else:
-            clear_consol()
+            clear_console()
 
 if __name__ == "__main__":
     main()
